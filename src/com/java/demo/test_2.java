5578194fd5c4b87184f5b8ddf8f8243c059c3280
Xcp <1695640389@qq.com>
2022-04-23 23:12:49 +0800

    sum=45

--- src/com/java/demo/test_2.java
+++ src/com/java/demo/test_2.java
@@ -1,5 +1,11 @@
 package com.java.demo;
 
 public class test_2 {
-    
+    public static void main(String[] args){
+        int sum=0;
+        for (int i=1;i<10;i++){
+            sum+=i;
+        }
+        System.out.println("sum:"+sum);
+    }
 }
